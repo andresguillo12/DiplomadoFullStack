d77a18f908f6d6af8d5e8641015af6577f0e0087
andresguillo12 <andresguillo5@gmail.com>
2019-10-10 19:45:18 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { findIconDefinition } from '@fortawesome/fontawesome-svg-core'
+import * as $ from 'jquery';
+
+library.add(fas, fab)
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+
+export class LayoutComponent implements OnInit {
+
+
+  homeIcon = findIconDefinition({ prefix: 'fas', iconName: 'home' });
+  menuIcon = findIconDefinition({ prefix: 'fas', iconName: 'align-justify' });
+  angularIcon = findIconDefinition({ prefix: 'fab', iconName: 'angular' });
+  promotionIcon = findIconDefinition({ prefix: 'fas', iconName: 'box-open' });
+  categoriesIcon = findIconDefinition({ prefix: 'fas', iconName: 'cubes' });
+  historyIcon = findIconDefinition({ prefix: 'fas', iconName: 'history' });
+  helpIcon = findIconDefinition({ prefix: 'fas', iconName: 'question-circle' });
+
+  constructor() { }
+
+  ngOnInit() {
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  }
+
+}
